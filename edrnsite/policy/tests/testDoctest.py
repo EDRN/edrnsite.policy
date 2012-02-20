152fc7851cf7b7e78c937aff930d0a4cf904e766
Sean Kelly <sean.c.kelly@me.com>
2012-02-20 22:01:21 +0000

    use fixture

--- edrnsite/policy/tests/testDoctest.py
+++ edrnsite/policy/tests/testDoctest.py
@@ -1,21 +1,21 @@
 # encoding: utf-8
-# Copyright 2009 California Institute of Technology. ALL RIGHTS
+# Copyright 2009–2012 California Institute of Technology. ALL RIGHTS
 # RESERVED. U.S. Government Sponsorship acknowledged.
 
 '''EDRN Site Policy: functional and documentation tests.
 '''
 
-from Testing import ZopeTestCase as ztc
-import unittest, doctest, base
+import doctest
+import unittest2 as unittest
+from plone.testing import layered
+from edrnsite.policy.testing import EDRNSITE_POLICY_FUNCTIONAL_TESTING as LAYER
+
+optionFlags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE | doctest.REPORT_ONLY_FIRST_FAILURE)
 
 def test_suite():
-	return unittest.TestSuite([
-		ztc.ZopeDocFileSuite('README.txt', package='edrnsite.policy',
-			test_class=base.EDRNSitePolicyFunctionalTestCase,
-			optionflags=doctest.REPORT_ONLY_FIRST_FAILURE | doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
-	])
-	
+    return unittest.TestSuite([
+        layered(doctest.DocFileSuite('README.txt', package='edrnsite.policy', optionflags=optionFlags), LAYER),
+    ])
 
 if __name__ == '__main__':
-	unittest.main(defaultTest='test_suite')
-	
+    unittest.main(defaultTest='test_suite')
