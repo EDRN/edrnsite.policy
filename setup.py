f3889c3ba178922161d84b3415cc05895ba16cbd
Sean Kelly <sean.c.kelly@me.com>
2014-06-13 10:35:28 -0500

    Licensing

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ _url         = 'http://cancer.jpl.nasa.gov/products/edrnsite-policy'
 _downloadURL = 'http://oodt.jpl.nasa.gov/dist/edrnsite'
 _author      = 'Sean Kelly'
 _authorEmail = 'sean.kelly@jpl.nasa.gov'
-_license     = 'Proprietary'
+_license     = 'ALv2'
 _namespaces  = ['edrnsite']
 _entryPoints = {'z3c.autoinclude.plugin': ['target=plone']}
 _extras      = {'test': ['plone.app.testing',]}
@@ -43,7 +43,7 @@ _classifiers = [
     'Framework :: Plone',
     'Intended Audience :: Healthcare Industry',
     'Intended Audience :: Science/Research',
-    'License :: Other/Proprietary License',
+    'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Topic :: Internet :: WWW/HTTP',
