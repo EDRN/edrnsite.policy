8f9bf4e3e8685e7546c0fa48ee68e045f3153003
Sean Kelly <kelly@seankelly.biz>
2014-06-26 21:01:57 -0500

    prepare release 1.2.11

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ import os.path
 _name        = 'edrnsite.policy'
 _version     = '1.2.11'
 _description = 'EDRN Public Portal Site Policy and Component Orchestration'
-_url         = 'http://cancer.jpl.nasa.gov/products/edrnsite-policy'
-_downloadURL = 'http://oodt.jpl.nasa.gov/dist/edrnsite'
 _author      = 'Sean Kelly'
 _authorEmail = 'sean.kelly@jpl.nasa.gov'
 _license     = 'ALv2'
@@ -59,7 +57,7 @@ def _read(*rnames):
 
 _header = '*' * len(_name) + '\n' + _name + '\n' + '*' * len(_name)
 _longDescription = _header + '\n\n' + _read('README.rst') + '\n\n' + _read('docs', 'INSTALL.txt') + '\n\n' \
-    + _read('docs', 'HISTORY.txt') + '\n\n' + _read('docs', 'LICENSE.txt')
+    + _read('docs', 'HISTORY.txt') + '\n'
 open('doc.txt', 'w').write(_longDescription)
 _cp = SafeConfigParser()
 _cp.read([os.path.join(os.path.dirname(__file__), 'setup.cfg')])
@@ -70,7 +68,6 @@ setup(
     author_email=_authorEmail,
     classifiers=_classifiers,
     description=_description,
-    download_url=_downloadURL,
     entry_points=_entryPoints,
     extras_require=_extras,
     include_package_data=True,
@@ -81,7 +78,7 @@ setup(
     name=_name,
     namespace_packages=_namespaces,
     packages=find_packages(exclude=['ez_setup']),
-    url=_url,
+    url='https://github.com/EDRN/' + _name,
     version=_version,
     zip_safe=_zipSafe,
 )
