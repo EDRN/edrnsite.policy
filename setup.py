27a33719504a47c945d0e30c3b95f41882c9a6b9
Sean Kelly <kelly@seankelly.biz>
2014-06-26 21:02:55 -0500

    bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import os.path
 # ------------
 
 _name        = 'edrnsite.policy'
-_version     = '1.2.11'
+_version     = '1.2.12'
 _description = 'EDRN Public Portal Site Policy and Component Orchestration'
 _author      = 'Sean Kelly'
 _authorEmail = 'sean.kelly@jpl.nasa.gov'
