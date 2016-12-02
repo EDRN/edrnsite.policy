bcf394996d61625c5a7cf1c9afab6a41cd11f6c2
Sean Kelly <kelly@seankelly.biz>
2016-12-02 17:04:07 -0600

    Move CA-1436 from 1.2.18 (which is already released) to 1.2.19; add CA-1436; add additioanl upgrade steps

--- edrnsite/policy/upgrades.py
+++ edrnsite/policy/upgrades.py
@@ -858,12 +858,38 @@ def upgrade11to12(setupTool):
     transaction.commit()
     _logger.info('Upgrade 11-to-12 complete')
 
-#only upgrade eke.ecas for the latest dataset faceted page
+
+# only upgrade eke.ecas for the latest dataset faceted page AND fix collaborative groups again
 def upgrade12to13(setupTool):
     _logger.info('Upgrading EDRN Public Portal from profile version 12 to profile version 13')
     portal = _getPortal(setupTool)
+    request = portal.REQUEST
+    catalog, setup = getToolByName(portal, 'portal_catalog'), getToolByName(portal, 'portal_setup')
+    _logger.info("Disabling schema extender's cache")
+    from archetypes.schemaextender.extender import disableCache
+    disableCache(request)
+    propTool = getToolByName(portal, 'portal_properties')
+    propTool.site_properties.getProperty('enable_link_integrity_checks', True)
+    contentRuleStorage = getUtility(IRuleStorage)
+    _logger.info('Disabling link integrity checks')
+    propTool.site_properties.manage_changeProperties(enable_link_integrity_checks=False)
+    _logger.info('Disabling content rules')
+    contentRuleStorage.active = False
     qi = getToolByName(portal, 'portal_quickinstaller')
     qi.upgradeProduct('eke.ecas')
+    _logger.info('Ingesting everything fully')
+    setAutoIngestProperties(portal)
+    portal.unrestrictedTraverse('@@ingestEverythingFully')()
+    _logger.info('Clearing ingest paths to prevent automatic ingest')
+    if portal.hasProperty('edrnIngestPaths'):
+        portal.manage_delProperties(['edrnIngestPaths'])
+    _logger.info('Resetting portal "from" email address')
+    portal.manage_changeProperties(email_from_address='sean.kelly@jpl.nasa.gov')
+    _logger.info('Clearing, finding, and re-building the catalog')
+    catalog.clearFindAndRebuild()
+    uidCatalog = getToolByName(portal, 'uid_catalog')
+    _logger.info('Rebuilding the UID catalog')
+    uidCatalog.manage_rebuildCatalog()
     transaction.commit()
     _logger.info('Upgrade 12-to-13 complete')
 
